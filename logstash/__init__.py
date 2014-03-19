b8b6d70bfd93e131effe41758ee2154c09bb7a86
Michael Stella <michael@jwplayer.com>
2014-03-19 10:25:11 -0400

    Reorganizing the structure of this project so there can be other scripts using a common library.
    Updated setup.py to remove unnecessary 2to3 call, and to use setuptools.
    
    [#60972274]

---
+++ logstash/__init__.py
@@ -0,0 +1,64 @@
+__author__    = 'Michael Stella <michael@jwplayer.com>'
+__copyright__ = "Copyright (c) 2013-2014 Long Tail Ad Solutions"
+__version__   = "1.0"
+
+import json
+import logging
+import os
+import redis
+import sys
+
+log_output=logging.getLogger('output')
+
+class Sink(object):
+    """Output sink base class"""
+
+    def log(self, **kwargs):
+        """Overload this to log a message and handle errors"""
+        pass
+
+
+class StdoutSink(Sink):
+    """Output to STDOUT in JSON format"""
+
+    def log(self, **kwargs):
+        """Log a message"""
+        print(json.dumps(kwargs))
+
+
+class RedisSink(Sink):
+    """Output to Redis"""
+
+    def __init__(self, host, key, port=6379):
+        self.host = host
+        self.port = port
+        self.key = key
+        self._connect()
+
+    def _connect(self):
+        self._conn = redis.StrictRedis(
+                        host=self.host,
+                        port=self.port,
+                        db=0,
+                        socket_timeout=30)
+        self._conn.ping()
+
+
+    def log(self, **kwargs):
+        """Log a message"""
+        try:
+            self._conn.rpush(self.key, json.dumps(kwargs))
+        except redis.exceptions.ConnectionError as e1:
+            try:
+                self._connect()
+                log_output.info("Redis: reconnected to server {0}:{1}".format(self.host, self.port))
+                self._conn.rpush(self.key, json.dumps(kwargs))
+            except Exception as e2:
+                log_output.error("Redis: {0}".format(e2))
+
+
+    def ping(self):
+        """Ping the server.  Boolean."""
+        return self._conn.ping()
+
+
--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 import sys
-if sys.version < '3':
-    from distutils.command.build_py import build_py
-else:
-    from distutils.command.build_py import build_py_2to3 as build_py
 
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
-    cmdclass        = {'build_py': build_py},
-    name            = 'logstash-forwarder',
+    name            = 'logstash-tools',
     version         = '1.0',
-    description     = 'Redis-based log forwarder for Logstash',
+    description     = 'A collection of tools for use with logstash',
+    long_description= open('README').read(),
+    url             = 'https://github.com/JWPlayer/logstash-tools',
     author          = 'Michael Stella',
     author_email    = 'michael@jwplayer.com',
-    scripts         = ['logstash-forwarder'],
-    requires        = ['redis', 'pyinotify'],
+    scripts         = ['logfile-forwarder'],
+    install_requires= ['redis', 'pyinotify'],
+    classifiers     = [
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+    ]
 )
 
